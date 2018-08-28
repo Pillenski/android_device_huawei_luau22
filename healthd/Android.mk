f14b0161762647dd2ecf3b831e4e14517dfce27c
Pillenski <pillenski@x10ded.org>
2018-08-28 20:33:06 +0000

    minor fixes

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_mtk.cpp
 LOCAL_MODULE := libhealthd.mtk
 LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
+LOCAL_C_INCLUDES := system/core/healthd/include/healthd bootable/recovery/minui/include system/core/base/include
 include $(BUILD_STATIC_LIBRARY)
 
 endif
